a217bcf4a01a8faaab54fdaa3b6afd015be5e684
mrkadirov2005 <muzaffar571181@gmail.com>
2025-02-26 00:55:18 +0500

    yeh

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -6,7 +6,7 @@ interface PROPS{
 export default function Video({src,lesson,teacher}:PROPS) {
   return (
     <div className="w-[500px] bg-gradient-to-tr from-gray-900 to-orange-800 p-2 rounded-md text-white flex flex-col items-start justify-between gap-5">
-    <iframe width="480" height="315" src={src} ></iframe>
+    <iframe className="w-full h-80"  src={src} ></iframe>
     <h1 className="text-2xl">{lesson}</h1>
     <h6 className="text-xl">{teacher}</h6>
   </div>
