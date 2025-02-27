ff8361514968763bab59350ba6616afc28eddd41
mrkadirov2005 <muzaffar571181@gmail.com>
2025-02-27 12:54:34 +0500

    done

--- src/components/Course_card.tsx
+++ src/components/Course_card.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@mui/material"
-
+import "./responsive.css"
 interface PROPS{
     name:string,
     description:string,
@@ -10,12 +10,12 @@ interface PROPS{
 }
 export default function Course_card({name,description,price,teacher,levels,index}:PROPS) {
   return (
-    <div id={`${index}`} className="w-[350px]  hover:scale-115 text-gray-950 shadow-2xl shadow-gray-950 h-fit gap-5 px-5 flex flex-col items-start justify-start min-h-[300px] bg-gradient-to-tr from-orange-400 to-gray-600 rounded-md p-2">
+    <div id={`${index}`} className="w-[350px] card_item_course  hover:scale-115 text-gray-950 shadow-2xl shadow-gray-950 h-fit gap-5 px-5 flex flex-col items-start justify-start min-h-[300px] bg-gradient-to-tr from-orange-400 to-gray-600 rounded-md p-2">
         <h1 className="w-full text-left text-2xl border-b-4 border-gray-900 p-2 ">
             {index+1 } {name}
         </h1>
         <span className="text-xl border-b-4 border-gray-950">Narx: {price}</span>
-        <p className="w-[300px] border-b-4 border-gray-950 pb-2 h-fit text-xl text-wrap flex flex-wrap">
+        <p className="w-[300px] course_card_description border-b-4 border-gray-950 pb-2 h-fit text-xl text-wrap flex flex-wrap">
           Tavsifi: {description}  
         </p>
         <h1 className="w-full border-b-4 border-gray-950">
--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,5 +1,34 @@
+import { Card } from "@mui/material";
+import { FaHeading } from "react-icons/fa";
+
 export default function Test() {
   return (
-    <div>Test</div>
+    <section className="w-full bg-gradient-to-tr from-gray-600 to-gray-950  min-h-screen ">
+      
+      <ul className=" flex flex-wrap gap-10 pt-20 w-full items-center justify-center">
+      
+          <Card_Test levels="A1-C1" number="20" test="Ingliz tili"/>
+          <Card_Test levels="A1-C1" number="20" test="Ingliz tili"/>
+          <Card_Test levels="A1-C1" number="20" test="Ingliz tili"/>
+          <Card_Test levels="A1-C1" number="20" test="Ingliz tili"/>
+          <Card_Test levels="A1-C1" number="20" test="Ingliz tili"/>
+          <Card_Test levels="A1-C1" number="20" test="Ingliz tili"/>
+          <Card_Test levels="A1-C1" number="20" test="Ingliz tili"/>
+      </ul>
+    </section>
+  )
+}
+interface PROPS{
+  test:string,
+  number:string,
+  levels:string,
+}
+function Card_Test({test,number,levels}:PROPS){
+  return(
+    <div className="w-[400px] rounded-md p-5 flex flex-col gap-2 bg-gradient-to-bl text-white  from-gray-600 to-gray-950 shadow-2 shadow-gray-200 ">
+    <h1 className="flex text-2xl items-center w-full  pb-2 gap-2 border-b-4 border-gray-800"><FaHeading/>{test}</h1>
+    <h1 className="flex text-2xl items-center w-full  pb-2 gap-2 border-b-4 border-gray-800">Testlar soni: {number}</h1>
+    <span className="flex text-lg">Darajalar: {levels}</span>
+  </div>
   )
 }
