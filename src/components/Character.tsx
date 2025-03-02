ee61fddc749f543d94f1aab72099f15c94cf568f
mrkadirov2005 <muzaffar571181@gmail.com>
2025-03-02 15:05:00 +0500

    updated

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -4,8 +4,8 @@ interface PROPS{
 }
 export default function Character({heading,size}:PROPS) {
   return (
-    <div className={`data_item flex flex-col text-white p-5 h-[100px] m-10 items-center justify-around bg-gray-800 ${size=="full"?"w-[80%]":"w-[300px]"}  hover:scale-110 rounded-md`}>
-        <h1 className='w-full text-xl   p-2 '>
+    <div className={`data_item flex flex-col text-white p-5 h-[100px]  m-10 items-center justify-around bg-gray-800 ${size=="full"?"w-[80%]":"w-[300px]"}  hover:scale-110 rounded-md`}>
+        <h1 className='w-full    p-2 '>
         {heading}
         </h1>
         
