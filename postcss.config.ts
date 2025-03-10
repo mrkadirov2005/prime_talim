6d51fa653040ccb16cb2316cb5775a1a94035ef6
mrkadirov2005 <muzaffar571181@gmail.com>
2025-03-10 10:14:43 +0500

    donesd

---
+++ postcss.config.ts
@@ -0,0 +1,9 @@
+const config = {
+    plugins: {
+      '@tailwindcss/postcss': {}, // Ensure this is updated
+      autoprefixer: {},
+    },
+  };
+  
+  export default config;
+  
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()]
-  
-
-})
-
-
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          vendor: ['lodash', 'axios'], // Example: split common libraries
+        },
+      },
+    },
+  },
+});
